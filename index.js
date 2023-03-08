48ab8c62ed99837816b79f12b249f99012a778eb
aaokweb <76078786+aaokweb@users.noreply.github.com>
2023-03-08 15:31:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
+
 app.use('/api', searchRouter);
 app.use('/safe', (req, res) => {
 	res.status(200).json({
